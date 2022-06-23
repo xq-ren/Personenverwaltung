4354b009be23ff482e588fa29329efc4e17702aa
Tabea Bolliger <tabea.bolliger@emea.merkleinc.com>
2022-06-23 10:33:53 +0200

    added test file

--- src/main/java/ch/bbw/PersonenverwaltungApplikation.java
+++ src/main/java/ch/bbw/PersonenverwaltungApplikation.java
@@ -1,5 +1,8 @@
 package ch.bbw;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class PersonenverwaltungApplikation {
     public static void main(String[] args) {
 
