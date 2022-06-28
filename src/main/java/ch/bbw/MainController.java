65af5820e9d25f863137861cb5030b6e9eaffc34
Tabea Bolliger <tabea.bolliger@emea.merkleinc.com>
2022-06-28 11:55:01 +0200

    still getting white label error page, idk why

--- src/main/java/ch/bbw/MainController.java
+++ src/main/java/ch/bbw/MainController.java
@@ -1,7 +1,13 @@
 package ch.bbw;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public class MainController {
+
+    @GetMapping("/person")
+    public String person(Model model){
+        return "person";
+    }
 
 }
